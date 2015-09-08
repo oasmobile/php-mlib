68ed9747f49f0dc966076e35cf0211973666dbb1
Minhao Zhang <minhao.zhang@qq.com>
2015-09-08 11:39:23 +0800

    + aws/aws-sdk-php + AwsWrappers namespace and Utilities class

---
+++ src/AwsWrappers/Utilities.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: minhao
+ * Date: 2015-09-08
+ * Time: 11:29
+ */
+namespace MLib\AwsWrapper;
+
+class Utilities
+{
+    static public function formatObjectToRedshiftLine($obj, $fields)
+    {
+        $patterns     = [
+            "/\\\\/",
+            "/\n/",
+            "/\r/",
+            "/\\|/",
+        ];
+        $replacements = [
+            "\\\\\\\\",
+            "\\\n",
+            "\\\r",
+            "\\|",
+        ];
+
+        $line = '';
+        foreach ($fields as $k) {
+            if ($line !== '') $line .= "|";
+
+            $v = $obj->$k;
+            $v = preg_replace($patterns, $replacements, $v);
+            $line .= $v;
+        }
+
+        return $line;
+    }
+
+}
