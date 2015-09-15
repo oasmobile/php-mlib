a964cd5a28ee03e7d7750c07c0be85ce886792c9
Minhao Zhang <minhao.zhang@qq.com>
2015-09-15 10:52:39 +0800

    * backward compatible inc-file, for logging purpose

--- src/Logging/logger.inc.php
+++ src/Logging/logger.inc.php
@@ -1,11 +1,61 @@
 <?php
 /**
  * Created by PhpStorm.
+ *
+ * For backward compatibility with old mlib
+ *
+ * This file provides global functions to log messages in various log-levels
+ *
+ * To alter logpath, min-log-level, as well as other monolog related functionalities, please refer to:
+ *      Oasis\Mlib\Logging\Logger
+ *
  * User: minhao
  * Date: 2015-09-15
  * Time: 10:43
  */
+use Oasis\Mlib\Logging\Logger;
+
 function mdebug($msg, $context = [])
 {
-    \Oasis\Mlib\Logging\Logger::debug($msg, $context);
+    Logger::debug($msg, $context);
+}
+
+function minfo($msg, $context = [])
+{
+    Logger::info($msg, $context);
+}
+
+function mwarning($msg, $context = [])
+{
+    Logger::warning($msg, $context);
+}
+
+function mnotice($msg, $context = [])
+{
+    Logger::notice($msg, $context);
+}
+
+function merror($msg, $context = [])
+{
+    Logger::error($msg, $context);
+}
+
+function mcritical($msg, $context = [])
+{
+    Logger::critical($msg, $context);
+}
+
+function malert($msg, $context = [])
+{
+    Logger::alert($msg, $context);
+}
+
+function memergency($msg, $context = [])
+{
+    Logger::emergency($msg, $context);
+}
+
+function mtrace(\Exception $e, $logLevel = Logger::INFO)
+{
+    Logger::log($logLevel, Logger::getExceptionDebugInfo($e));
 }
