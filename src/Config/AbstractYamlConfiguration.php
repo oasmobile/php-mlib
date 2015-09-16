c38968d1ed4d179062b7d914c860ece7243c8c0b
Minhao Zhang <minhao.zhang@qq.com>
2015-09-16 15:48:13 +0800

    + yaml config base

---
+++ src/Config/AbstractYamlConfiguration.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: minhao
+ * Date: 2015-09-16
+ * Time: 14:33
+ */
+namespace Oasis\Mlib\Config;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Yaml\Yaml;
+
+abstract class AbstractYamlConfiguration implements ConfigurationInterface
+{
+    protected $processedConfig = [];
+
+    public static function instance()
+    {
+        static $instances = [];
+        if ($instances[static::class] === null) {
+            $instances[static::class] = new static();
+        }
+
+        return $instances[static::class];
+    }
+
+    public function loadYaml($filename, $directories)
+    {
+        $locator   = new FileLocator($directories);
+        $yamlFiles = $locator->locate($filename, null, false);
+
+        $rawData = [];
+        foreach ($yamlFiles as $file) {
+            $config    = Yaml::parse($file);
+            $rawData[] = $config;
+        }
+
+        $defProcessor          = new Processor();
+        $this->processedConfig = $defProcessor->processConfiguration($this, $rawData);
+
+        $this->assignProcessedConfig();
+    }
+
+    abstract public function getConfigTreeBuilder();
+
+    /**
+     * Assigns processed config to specific properties
+     *
+     * @return mixed
+     */
+    abstract public function assignProcessedConfig();
+}
