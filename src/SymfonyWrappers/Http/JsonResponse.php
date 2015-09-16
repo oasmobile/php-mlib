b6514ad6720510c57344994657e4b4f10dd82292
Minhao Zhang <minhao.zhang@qq.com>
2015-09-16 21:13:39 +0800

    * fix: \Exception used in JsonRepsonse handler

--- src/SymfonyWrappers/Http/JsonResponse.php
+++ src/SymfonyWrappers/Http/JsonResponse.php
@@ -7,14 +7,13 @@
  */
 namespace Oasis\Mlib\SymfonyWrappers\Http;
 
-use League\Flysystem\Exception;
 use Symfony\Component\HttpFoundation\Request;
 
 class JsonResponse extends \Symfony\Component\HttpFoundation\JsonResponse
 {
     public function prepare(Request $request)
     {
-        set_exception_handler(function (Exception $e) {
+        set_exception_handler(function (\Exception $e) {
             $this->setStatusCode(self::HTTP_INTERNAL_SERVER_ERROR);
             $this->setData([
                                "rc"      => ($e->getCode() == 0 ? 0xffff : $e->getCode()),
